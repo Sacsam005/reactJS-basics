90100c6e693d77a38c9a9dccd6b0e18dd5e9a9c9
Sachin Samal <88958992+Sacsam005@users.noreply.github.com>
2021-12-18 03:15:34 -0600

    Update TextForm.js

--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -12,30 +12,40 @@ export default function TextForm(props) {
     setText(event.target.value);
   };
 
-  const removeChange = () => {
+  const removeChange = (event) => {
     setText("");
   };
 
   const [text, setText] = useState("");
 
   return (
-    <div className="container">
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          value={text}
-          onChange={handleOnChange}
-          className="form-control"
-          id="myBox"
-          rows="8"
-        ></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            value={text}
+            onChange={handleOnChange}
+            className="form-control"
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary" onClick={handleUpClick}>
+          Convert to UPPERCASE
+        </button>
+        <button className="btn btn-danger mx-4">Remove</button>
       </div>
-      <button className="btn-primary" onClick={handleUpClick}>
-        Convert to UPPERCASE
-      </button>
-      <button className="btn-danger mx-4" onClick={removeChange}>
-        Remove
-      </button>
-    </div>
+      <div className="container my-3">
+        <button className="btn btn-info">Your text summary</button>
+        <p>
+          {text.split(" ").length} <i>words</i> and {text.length}{" "}
+          <i>characters</i>
+        </p>
+        <p>{(0.008 * text.split(" ").length).toFixed(1)} minutes read time</p>
+        <h3>Preview</h3>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
