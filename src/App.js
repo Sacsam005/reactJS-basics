0386630e2b9286394f4d60d4771bd13a18d086f5
Sachin Samal <88958992+Sacsam005@users.noreply.github.com>
2021-12-18 21:16:50 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,65 @@
 import "./App.css";
+import React, { useState } from 'react'
+
 import Navbar from "./component/Navbar";
+import About from "./component/About";
 import TextForm from "./component/TextForm";
+import Alert from "./component/Alert";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
+  const [mode, setMode] = useState('light')
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000)
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = "#042743";
+      document.body.style.color = "white";
+      showAlert("DarkMode has been enabled", "Success");
+      document.title = "reactApp - Dark Mode";
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+      showAlert("LightMode has been enabled", "Success");
+      document.title = "reactApp - Light Mode";
+
+    }
+  }
+
   return (
     <>
-      <Navbar title="ReactApp" />
-      <TextForm heading="Convert the following text" />
+
+      <Router>
+        <Navbar title="ReactApp" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Switch>
+            <Route exact path="/about" element={<About />}>
+              <About />
+            </Route>
+            <Route exact path="/">
+              <TextForm showAlert={showAlert} heading="Convert the following text" mode={mode} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </>
   );
 }
