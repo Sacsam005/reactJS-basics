282025ef6da71bc0387d0cd22eceea86cbe60db3
Sachin Samal <88958992+Sacsam005@users.noreply.github.com>
2021-12-18 21:16:28 -0600

    Add files via upload

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,53 +1,45 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a className="navbar-brand" href="/">
-        {props.title}
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+    <div className="container">
+      <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+        <Link className="navbar-brand" to="/">
+          {props.title}
+        </Link>
+        <button className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="/">
-              HOME
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/">
-              ABOUT
-            </a>
-          </li>
-        </ul>
-        <form className="form-inline my-2 my-lg-0">
-          <input
-            className="form-control mr-sm-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button
-            className="btn btn-outline-success my-2 my-sm-0"
-            type="submit"
-          >
-            Search
-          </button>
-        </form>
-      </div>
-    </nav>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/">
+                HOME
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/About">
+                ABOUT
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+          <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode} />
+          <label className="form-check-label" htmlFor="flexSwitchCheckDefault">DarkMode</label>
+        </div>
+      </nav>
+    </div>
   );
 }
 
--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -3,8 +3,11 @@ import React, { useState } from "react";
 export default function TextForm(props) {
   const handleUpClick = () => {
     console.log(text);
+
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert('Text changed to UPPERCASE', 'success');
+
   };
 
   const handleOnChange = (event) => {
@@ -12,8 +15,26 @@ export default function TextForm(props) {
     setText(event.target.value);
   };
 
+  const handleLowerCase = (event) => {
+    let lowerCase = text.toLowerCase();
+    setText(lowerCase);
+    props.showAlert('Text changed to lowercase', 'success');
+  }
+
+  const copyText = (event) => {
+    let copiedText = document.getElementById('myBox');
+    let extraSpaces = text.split(/[ ] +/);
+    setText(extraSpaces.join(' '));
+    copiedText.select();
+    navigator.clipboard.writeText(copiedText.value);
+    props.showAlert('Copied to Clipboard', 'success');
+
+  }
+
   const removeChange = (event) => {
     setText("");
+    props.showAlert('TextBox cleared', 'success');
+
   };
 
   const [text, setText] = useState("");
@@ -25,6 +46,7 @@ export default function TextForm(props) {
         <div className="mb-3">
           <textarea
             value={text}
+            style={{ backgroundColor: props.mode === 'light' ? 'white' : 'grey' }}
             onChange={handleOnChange}
             className="form-control"
             id="myBox"
@@ -34,18 +56,35 @@ export default function TextForm(props) {
         <button className="btn btn-primary" onClick={handleUpClick}>
           Convert to UPPERCASE
         </button>
-        <button className="btn btn-danger mx-4">Remove</button>
+        <button className="btn btn-secondary" onClick={handleLowerCase}>
+          Convert to lowercase
+        </button>
+        <button className="btn btn-success" onClick={copyText}>
+          Copy Text to clipboard
+        </button>
+        <button className="btn btn-danger" onClick={removeChange}>
+          Remove
+        </button>
       </div>
       <div className="container my-3">
-        <button className="btn btn-info">Your text summary</button>
         <p>
-          {text.split(" ").length} <i>words</i> and {text.length}{" "}
-          <i>characters</i>
+          <a className="btn btn-info" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-controls="collapseExample">
+            Your text summary
+          </a>
         </p>
-        <p>{(0.008 * text.split(" ").length).toFixed(1)} minutes read time</p>
+        <div className="collapse" id="collapseExample">
+          <p className="my-2">
+            {text.split(" ").length} <i>words</i> and {text.length}{" "}
+            <i>characters</i>
+          </p>
+          <p>{(0.008 * text.split(" ").length).toFixed(1)} minutes read time</p>
+        </div>
+
         <h3>Preview</h3>
-        <p>{text}</p>
+        <p>{text.length > 0 ? text : "Enter something in the text box to preview here"}</p>
       </div>
+
+
     </>
   );
 }
