f93bf2e4783043defda4024165776b03f8074e19
Sachin Samal <88958992+Sacsam005@users.noreply.github.com>
2021-12-22 01:39:28 -0600

    Create Changestate.js

---
+++ src/Changestate.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+export class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      loggedIn: true
+    }
+
+    this.logIn = this.logIn.bind(this)
+  }
+
+  logIn() {
+    this.setState(prevState => {
+      return {
+        loggedIn: !prevState.loggedIn
+      }
+    })
+  }
+
+  render() {
+    let buttonText = this.state.loggedIn ? "LogIn" : "LogOut";
+
+    return (
+      <>
+        <button className="btn btn-primary" onClick={this.logIn}>{buttonText}</button>
+      </>
+    )
+  }
+}
+
+export default App
